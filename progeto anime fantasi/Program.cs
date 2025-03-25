3913d5238d9e410f1a185194f43a4efddd07d33b
LEANDRO PASSOS DA SILVA FILHO <leandro.psfilho@senacsp.edu.br>
2025-03-25 09:26:22 -0300

    "Mudei para a tela inicial"

--- progeto anime fantasi/Program.cs
+++ progeto anime fantasi/Program.cs
@@ -16,7 +16,7 @@ namespace progeto_anime_fantasi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmTelaInicial());
         }
     }
 }
